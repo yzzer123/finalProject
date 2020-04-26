024c7c814b757b0553293a63c59033c4a6ec5dd9
yzzer123 <13055686620@139.com>
2020-04-26 11:43:31 +0800

    版本改变0.61.5，安装navigation库 element库 wevview库

--- teachAssist/App.js
+++ teachAssist/App.js
@@ -1,42 +1,91 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-import 'react-native-gesture-handler';
-import React from 'react';
-import {NavigationContainer} from '@react-navigation/native'
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
+import * as React from 'react';
+import { View, Text, Button,
+  TextInput,
+
 } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import   {createStackNavigator}  from '@react-navigation/stack';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+function CreatePostScreen({ navigation, route }) {
+  const [postText, setPostText] = React.useState('');
 
-const App = () => {
+  return (
+    <>
+      <TextInput
+        multiline
+        placeholder="What's on your mind?"
+        style={{ height: 200, padding: 10, backgroundColor: 'white' }}
+        value={postText}
+        onChangeText={setPostText}
+      />
+      <Button
+        title="Done"
+        onPress={() => {
+          // Pass params back to home screen
+          navigation.navigate('Home', { post: postText });
+        }}
+      />
+    </>
+  );
+}
+const Home = ({route,navigation})=>{
+  React.useEffect(() => {
+    if (route.params?.post) {
+      // Post updated, do something with `route.params.post`
+      // For example, send the post to the server
+      console.log("update");
+    }
+  }, [route.params?.post]);
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button
+        title="Create post"
+        onPress={() => navigation.navigate('CreatePost')}
+      />
+      <Text style={{ margin: 10 }}>Post: {route.params?.post}</Text>
+    </View>
+  );
+}
+function DetailsScreen({route, navigation }) {
+  const { itemId } = route.params;
+  const { otherParam } = route.params;
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+      <Text>itemId: {JSON.stringify(itemId)}</Text>
+      <Text>otherParam: {JSON.stringify(otherParam)}</Text>
+      <Button
+        title="Go to Details... again"
+        
+        onPress={() =>
+          navigation.push('Details', {
+            itemId: Math.floor(Math.random() * 100),
+          })
+        }
+      />
+        <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+
+const App = () =>{
+
+
   return (
     <NavigationContainer>
-       <Text>Hello</Text>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} options={{title:"Pupu"}}/>
+        <Stack.Screen name="Details" component={DetailsScreen}  initialParams={{ itemId: 42 }}/>
+        <Stack.Screen name="CreatePost" component={CreatePostScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
-};
 
-const styles = StyleSheet.create({
-  containner:{
-
-  }
-});
+}
 
 export default App;
